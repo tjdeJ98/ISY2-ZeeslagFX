560b33fefaf4561b445d10c49bb0de91ef9e7fa8
Just Bodewes <justbodewes@gmail.com>
2024-01-21 13:52:51 +0100

    Thread proof ServerConnection Singleton

--- src/main/java/hanze/isy2zeeslagfx/network/ServerConnection.java
+++ src/main/java/hanze/isy2zeeslagfx/network/ServerConnection.java
@@ -3,10 +3,21 @@ package hanze.isy2zeeslagfx.network;
 import java.net.Socket;
 
 public class ServerConnection {
-    //singleton pattern
-    private volatile Socket socket;
 
+    private volatile static ServerConnection instance;
 
+    private ServerConnection() {}
 
+    public static ServerConnection getInstance() {
 
+        if (instance == null) {
+            synchronized (ServerConnection.class) {
+                if (instance == null) {
+                    instance = new ServerConnection();
+                }
+            }
+        }
+        return instance;
+    }
 }
+
